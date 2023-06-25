4ece0de8696e1093eb64220bf6a93ad808ba2ea9
zypherscript <kitti.kthp@gmail.com>
2023-06-26 02:40:47 +0700

    try testIsolation for e2e

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,18 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('Tour of Heroes')
-  })
-})
+describe('Tour of Heroes App Testing', { testIsolation: false }, () => {
+  it(`Should have title 'Tour Of Heroes'`, () => {
+    cy.visit('/');
+    cy.contains('Tour of Heroes');
+  });
+
+  it(`Should have title 'Top Heroes'`, () => {
+    cy.contains('Top Heroes');
+  });
+
+  it(`Should have title 'Hero Search'`, () => {
+    cy.contains('Hero Search');
+  });
+
+  it(`Should have title 'Messages'`, () => {
+    cy.contains('Messages');
+  });
+});
