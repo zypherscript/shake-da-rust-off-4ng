7a6240623f614ce1dc671976553415a29efac1c5
zypherscript <kitti.kthp@gmail.com>
2023-07-02 00:16:49 +0700

    more test for hero service

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -7,6 +7,7 @@ import {
 import { HeroService } from './hero.service';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
+import { catchError, throwError } from 'rxjs';
 
 describe('HeroService', () => {
   let heroService: HeroService;
@@ -54,6 +55,166 @@ describe('HeroService', () => {
       const req = httpMock.expectOne('api/heroes');
       expect(req.request.method).toBe('GET');
       req.flush([], { status: 500, statusText: 'Internal Server Error' });
+    });
+  });
+
+  describe('getHero', () => {
+    it('should retrieve a hero by id from the API', () => {
+      const dummyHero: Hero = { id: 1, name: 'Hero 1' };
+
+      heroService.getHero(1).subscribe((hero) => {
+        expect(hero).toEqual(dummyHero);
+      });
+
+      const req = httpMock.expectOne('api/heroes/1');
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyHero);
+    });
+
+    it('should handle errors when retrieving a hero by id', () => {
+      heroService
+        .getHero(1)
+        .pipe(
+          catchError((error) => {
+            expect(error).toBeTruthy();
+            return throwError(() => error);
+          })
+        )
+        .subscribe();
+
+      const req = httpMock.expectOne('api/heroes/1');
+      expect(req.request.method).toBe('GET');
+      req.flush(null, { status: 500, statusText: 'Internal Server Error' });
+    });
+  });
+
+  describe('searchHeroes', () => {
+    it('should search for heroes by name in the API', () => {
+      const searchName = 'Hero';
+
+      const dummyHeroes: Hero[] = [
+        { id: 1, name: 'Hero 1' },
+        { id: 2, name: 'Hero 2' },
+      ];
+
+      heroService.searchHeroes(searchName).subscribe((heroes) => {
+        expect(heroes.length).toBe(2);
+        expect(heroes).toEqual(dummyHeroes);
+      });
+
+      const req = httpMock.expectOne(`api/heroes/?name=${searchName}`);
+      expect(req.request.method).toBe('GET');
+      req.flush(dummyHeroes);
+    });
+
+    it('should return an empty array when searching for heroes with empty search term', () => {
+      const emptySearch = '';
+
+      heroService.searchHeroes(emptySearch).subscribe((heroes) => {
+        expect(heroes.length).toBe(0);
+      });
+
+      httpMock.expectNone(`api/heroes/?name=${emptySearch}`);
+    });
+  });
+
+  describe('addHero', () => {
+    it('should add a hero to the API', () => {
+      const newHero: Hero = { id: 3, name: 'Hero 3' };
+
+      heroService.addHero(newHero).subscribe((hero) => {
+        expect(hero).toEqual(newHero);
+      });
+
+      const req = httpMock.expectOne('api/heroes');
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual(newHero);
+      req.flush(newHero);
+    });
+
+    it('should handle errors when adding a hero', () => {
+      const newHero: Hero = { id: 3, name: 'Hero 3' };
+
+      heroService
+        .addHero(newHero)
+        .pipe(
+          catchError((error) => {
+            expect(error).toBeTruthy();
+            return throwError(() => error);
+          })
+        )
+        .subscribe();
+
+      const req = httpMock.expectOne('api/heroes');
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual(newHero);
+      req.flush(null, { status: 500, statusText: 'Internal Server Error' });
+    });
+  });
+
+  describe('deleteHero', () => {
+    it('should delete a hero from the API', () => {
+      const heroId = 2;
+
+      heroService.deleteHero(heroId).subscribe((hero) => {
+        expect(hero.id).toBe(heroId);
+      });
+
+      const req = httpMock.expectOne(`api/heroes/${heroId}`);
+      expect(req.request.method).toBe('DELETE');
+      req.flush({ id: heroId });
+    });
+
+    it('should handle errors when deleting a hero', () => {
+      const heroId = 2;
+
+      heroService
+        .deleteHero(heroId)
+        .pipe(
+          catchError((error) => {
+            expect(error).toBeTruthy();
+            return throwError(() => error);
+          })
+        )
+        .subscribe();
+
+      const req = httpMock.expectOne(`api/heroes/${heroId}`);
+      expect(req.request.method).toBe('DELETE');
+      req.flush(null, { status: 500, statusText: 'Internal Server Error' });
+    });
+  });
+
+  describe('updateHero', () => {
+    it('should update a hero on the API', () => {
+      const updatedHero: Hero = { id: 1, name: 'Updated Hero' };
+
+      heroService.updateHero(updatedHero).subscribe((response) => {
+        expect(response).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne('api/heroes');
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toEqual(updatedHero);
+      req.flush(updatedHero);
+    });
+
+    it('should handle errors when updating a hero', () => {
+      const updatedHero: Hero = { id: 1, name: 'Updated Hero' };
+
+      heroService
+        .updateHero(updatedHero)
+        .pipe(
+          catchError((error) => {
+            expect(error).toBeTruthy();
+            return throwError(() => error);
+          })
+        )
+        .subscribe();
+
+      const req = httpMock.expectOne('api/heroes');
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toEqual(updatedHero);
+      req.flush(null, { status: 500, statusText: 'Internal Server Error' });
     });
   });
 });
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -29,17 +29,17 @@ export class HeroService {
   }
 
   /** GET hero by id. Return `undefined` when id not found */
-  getHeroNo404<Data>(id: number): Observable<Hero> {
-    const url = `${this.heroesUrl}/?id=${id}`;
-    return this.http.get<Hero[]>(url).pipe(
-      map((heroes) => heroes[0]), // returns a {0|1} element array
-      tap((h) => {
-        const outcome = h ? 'fetched' : 'did not find';
-        this.log(`${outcome} hero id=${id}`);
-      }),
-      catchError(this.handleError<Hero>(`getHero id=${id}`))
-    );
-  }
+  // getHeroNo404<Data>(id: number): Observable<Hero> {
+  //   const url = `${this.heroesUrl}/?id=${id}`;
+  //   return this.http.get<Hero[]>(url).pipe(
+  //     map((heroes) => heroes[0]), // returns a {0|1} element array
+  //     tap((h) => {
+  //       const outcome = h ? 'fetched' : 'did not find';
+  //       this.log(`${outcome} hero id=${id}`);
+  //     }),
+  //     catchError(this.handleError<Hero>(`getHero id=${id}`))
+  //   );
+  // }
 
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
